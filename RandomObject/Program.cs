bedfc1623cbc3713970ea141b4fb6f5f7de6841c
Oğuzhan Topbaş <topbasoguzhan@gmail.com>
2021-12-15 23:30:13 +0300

    Random object

--- RandomObject/Program.cs
+++ RandomObject/Program.cs
@@ -6,7 +6,28 @@ namespace RandomObject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random(); //instance
+
+            //a random number from 0-100
+            int number1 = rnd.Next(0, 100);
+            number1 = rnd.Next(0, 100);
+
+            //a random number from -10-20
+            int number2 = rnd.Next(-10, 20);
+            number2 = rnd.Next(-10, 20);
+
+            //a random number
+            int number3 = rnd.Next();
+
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+            Console.ReadLine();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(rnd.Next(1,50));
+            }
         }
     }
 }
