903d724fe3f461f44a08f17ba7652e9d00fba1e8
Oğuzhan Topbaş <topbasoguzhan@gmail.com>
2021-12-16 19:34:20 +0300

    Shapes with Array

---
+++ ArrayShapes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ArrayShapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] matrix = null;
+            try
+            {
+                Console.Write("Please enter the size: ");
+                int enterSize = int.Parse(Console.ReadLine());
+                matrix = new bool[enterSize, 2 * enterSize - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            for (int line = 0; line < matrix.GetLength(0); line++)
+            {
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    if ((line + column >= matrix.GetLength(0) - 1 && line - column <= matrix.GetLength(0) - 1))
+                    {
+                        matrix[line,column] = true;
+                    }
+                }
+            }
+
+            //write on screen
+            for (int line = 0; line < matrix.GetLength(0); line++)
+            {
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    Console.Write(matrix[line,column] ? "X" : " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Uzunluk: ");
+            int sideLength = Convert.ToInt32(Console.ReadLine());
+            int middlePoint = sideLength - 1;
+            int floor = sideLength * 2 - 1;
+
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int k = 0; k < floor; k++)
+                {
+                    if ((i + k >= middlePoint && k - i <= middlePoint))
+                    {
+                        Console.WriteLine("*");
+                    }
+                    else if (i == middlePoint)
+                        Console.WriteLine("*");
+                    else
+                        Console.WriteLine(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays
+            //We call the data types that we can hold more than one value of the same type as arrays.
+            //Index starts at zero
+
+            int number = 0;
+            int[] numbers = new[] { 1, 2, 3, 6, 69, 45 };
+            numbers = new int[10];
+
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 6;
+
+            Console.WriteLine(numbers.Length);
+
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(numbers[i]);
+            }
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            double[,] matrix = new double[5, 3];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.NextDouble() * rnd.Next(100);
+                    Console.WriteLine(matrix[i, j]);
+                }
+            }
+
+            Array.Resize(ref numbers, 20);
+
+            //ArrayList
+
+            ArrayList list1 = new ArrayList();
+            list1.Add(123);
+            list1.Add("Kamil");
+            list1.Add(true);
+            list1.Add(null);
+            list1.Add(DateTime.Now);
+            list1.Add(matrix);
+            Console.WriteLine(list1);
+
+            //Generic List
+
+            List<int> list2 = new List<int>();
+
+            list2.Add(5);
+            list2.Add(3);
+            list2.Add(23);
+
+
+
+
+        }
+    }
+}
