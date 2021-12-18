0f16f92e9480e020b632694061e575465a9400c9
Oğuzhan Topbaş <topbasoguzhan@gmail.com>
2021-12-19 00:09:18 +0300

    hangman game (not completed)

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HangmanGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int life = 5, known = 0;
+                string[] questions = { "zeytinburnu", "kahramanmaraş", "çanakkale", "hayrabolu" };
+                string[] selectedQuestion = questions[new Random().Next(questions.Length)];
+                double score = selectedQuestion.Length * 100;
+                char[] screen = new char[selectedQuestion.Length];
+                Console.Clear();
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    screen[i] = '-';
+                }
+
+                List<char> previousPredictions = new List<char>();
+
+                do
+                {
+                    //write on screen
+                    foreach (char character in screen)
+                    {
+                        Console.WriteLine(character + " ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine($"{score:#.00} score.{life} left alive");
+                    Console.WriteLine("Guess: ");
+                    string guess = Console.ReadLine();
+                    bool isitRight = false;
+                    if )
+                    {
+
+                    }
+
+                } while (true);
+            } while (true);
+        }
+    }
+}
