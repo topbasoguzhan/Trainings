cd17c4fc25cc6b1251e5974cdd7a7f6c147364d5
Oğuzhan Topbaş <topbasoguzhan@gmail.com>
2021-12-20 21:27:11 +0300

    Number guessing game completed.

---
+++ NumberGuessGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace NumberGuessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do//one game circle.
+            {
+                Random rnd = new Random();
+                int guessNumber = 0, randomNumber = rnd.Next(1, 101), entry = 0;
+                do
+                {
+                    Console.WriteLine("Guess random number: ");
+                    try
+                    {
+                        entry = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You made a wrong expression!");
+                        continue;
+                    }
+                    finally
+                    {
+                        guessNumber++;
+                    }
+
+                    if (entry == randomNumber)
+                    {
+                        Console.WriteLine($"Congratulations! You finished {guessNumber}. try :)");
+                        break;
+                    }
+                    else if (entry > randomNumber)
+                    {
+                        Console.WriteLine("Lower your guess...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Higher your guess...");
+                    }
+                } while (true);
+                Console.WriteLine("Press Y to play again. If you don't want just press any button :D!");
+                ConsoleKeyInfo info = Console.ReadKey();
+                if (info.Key != ConsoleKey.Y)
+                    break;
+            } while (true);
+        }
+    }
+}
